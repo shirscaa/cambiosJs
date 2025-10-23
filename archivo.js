bf23255b1096f7a1703bb6c7a530cd3e46369a15
unknown <sscamm04@gmail.com>
2025-10-23 15:44:06 -0500

    miPrimerCommit

---
+++ archivo.js
@@ -0,0 +1,18 @@
+let frutas = ['manzana','pera','uva']
+console.log(frutas)
+
+frutas.push('mango')
+console.log(frutas)
+
+frutas.shift()
+console.log(frutas)
+
+frutas.unshift('fresa')
+console.log(frutas)
+
+frutas.shift()
+
+let longitud = frutas.length
+console.log(longitud)
+
+console.log(frutas[frutas.length-1])
